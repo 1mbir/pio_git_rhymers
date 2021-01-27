c2c1d2df53b1736391d92d24fc40f06d5230fc5b
1mbir <mikoimb99@gmail.com>
2021-01-27 12:03:16 +0100

    Zrobiony punkt 2.2.8

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,8 +1,12 @@
 package edu.kis.vh.nursery;
 
+import edu.kis.vh.nursery.stack.IntArrayStack;
+import edu.kis.vh.nursery.stack.StackList;
+
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
-    private final DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
+    //private final DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
+    private final StackList temp = new IntArrayStack();
 
     public FIFORhymer() {
         super();
@@ -12,18 +16,18 @@ public class FIFORhymer extends DefaultCountingOutRhymer {
     public int countOut() {
         while (!callCheck())
 
-            getTemp().countIn(super.countOut());
+            getTemp().push(super.countOut());
 
-        int ret = getTemp().countOut();
+        int ret = getTemp().pop();
 
-        while (!getTemp().callCheck())
+        while (!getTemp().isEmpty())
 
-            countIn(getTemp().countOut());
+            countIn(getTemp().pop());
 
         return ret;
     }
 
-    public DefaultCountingOutRhymer getTemp() {
+    public StackList getTemp() {
         return temp;
     }
 }
